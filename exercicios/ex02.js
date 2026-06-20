08b9903f03c19b5bb18b19b718bc0f32acdf956d
robsonsoaresid <robemsuave@gmail.com>
2026-06-20 14:16:56 -0300

    Adiciona aplicacao de audio e estilos

--- exercicios/ex02.js
+++ exercicios/ex02.js
@@ -60,3 +60,12 @@ function alterarContexto(contexto) {
       break;
   }
 }
+
+
+
+const audioElement = new Audio('caminho/do/arquivo-de-audio.mp3');
+
+audioElement.play(); // Inicia a reprodução
+audioElement.pause(); // Pausa a reprodução
+audioElement.currentTime = 10; // Move para 10 segundos no áudio
+audioElement.volume = 0.5; // Define o volume para metade (50%)
